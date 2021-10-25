64bd8f385214b15679623d3fe8f74f5193997273
ezeibarra <ezequiel.ibarra@garajedeideas.com>
2021-10-25 19:53:34 -0300

    Add config db

--- src/settings/db.js
+++ src/settings/db.js
@@ -3,10 +3,12 @@ const mysql = require("mysql");
 
 //MYSQL
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "node20_mysql",
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b0d9e007154f51",
+  password: "263d3df8",
+  database: "heroku_9c4b36e44ec458e",
 });
 
 module.exports = connection;
+
+//mysql://b0d9e007154f51:263d3df8@us-cdbr-east-04.cleardb.com/heroku_9c4b36e44ec458e?reconnect=true
