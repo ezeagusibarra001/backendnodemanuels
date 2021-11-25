c9dc0d10d8656e4780da84a0071a2de1d2076d99
ezeibarra <ezequiel.ibarra@garajedeideas.com>
2021-11-25 17:33:18 -0300

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -35,34 +35,48 @@ const {
 } = require("../controllers/discounts");
 
 const app = express.Router();
-app.use(cors());
+var whitelist = ["http://localhost:3000", "http://manuels.com.ar"];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 //CLASES
 app.get("/", homeClases);
 app.get("/lessons", getClases);
 app.get("/lessons/:idLesson", getClasesById);
-app.post("/lessons", postClases);
+app.post("/lessons", cors(corsOptionsDelegate), postClases);
 app.delete("/lessons/:idLesson", deleteClases);
 //LOGIN
 app.post("/login", login);
 //PUBLICATIONS
 app.get("/publications", getBlog);
-app.post("/publications", fileUpload, postBlog);
+app.post("/publications", cors(corsOptionsDelegate), fileUpload, postBlog);
 app.delete("/publications/:idPublications", deleteBlog);
 //REVIEWS
 app.get("/reviews", getReviews);
-app.post("/reviews", postReviews);
+app.post("/reviews", cors(corsOptionsDelegate), postReviews);
 app.put("/reviews/accept/:idReview", validateTrue);
 app.delete("/reviews/:idReview", deleteReview);
 //CHECKOUT
 app.get("/payments", getPayments);
-app.post("/payments", fileUploadPayment, postPayments);
+app.post(
+  "/payments",
+  cors(corsOptionsDelegate),
+  fileUploadPayment,
+  postPayments
+);
 app.delete("/payments/:idPayment", deletePayments);
 app.put("/payments/:idPayment", putPayment);
 //DISCOUNTS
 app.put("/discounts", putDiscounts);
-app.post("/discounts", postDiscounts);
+app.post("/discounts", cors(corsOptionsDelegate), postDiscounts);
 app.get("/discounts", getDiscounts);
 app.delete("/discounts/:idDiscounts", deleteDiscounts);
 module.exports = app;
